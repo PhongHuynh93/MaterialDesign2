788a291fb37cae62b5ad2e4e96a69d8a12229e98
Phong <p7157568@gmail.com>
2016-05-16 23:30:48 +0700

    change main activity

--- app/src/main/java/dhbk/android/materialdesign/MainActivity.java
+++ app/src/main/java/dhbk/android/materialdesign/MainActivity.java
@@ -1,17 +1,26 @@
 package dhbk.android.materialdesign;
 
-
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import dhbk.android.materialdesign.activities.FragmentDrawer;
+import dhbk.android.materialdesign.activities.FriendsFragment;
+import dhbk.android.materialdesign.activities.HomeFragment;
+import dhbk.android.materialdesign.activities.MessagesFragment;
 
+// TODO: 5/16/16 5 thay đổi mainactivity
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
+
+    private static String TAG = MainActivity.class.getSimpleName();
 
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
@@ -24,14 +33,15 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(mToolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         drawerFragment = (FragmentDrawer)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+
+        // display the first navigation drawer view on app launch
+        displayView(0);
     }
 
     @Override
@@ -53,12 +63,48 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
             return true;
         }
 
+        if(id == R.id.action_search){
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
 
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new FriendsFragment();
+                title = getString(R.string.title_friends);
+                break;
+            case 2:
+                fragment = new MessagesFragment();
+                title = getString(R.string.title_messages);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
 
---
+++ app/src/main/java/dhbk/android/materialdesign/activities/FriendsFragment.java
@@ -0,0 +1,10 @@
+package dhbk.android.materialdesign.activities;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by huynhducthanhphong on 5/16/16.
+ */
+// TODO: 5/16/16 3 tạo friend fragment
+public class FriendsFragment extends Fragment{
+}
